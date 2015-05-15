2e9fe564c989d14fdf731cd93238905fe1e338d5
Rojam06 <rojam06@gmail.com>
2015-05-15 08:20:15 -0500

    Add xbee instruction to receiver

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,24 +1,19 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+#include <SoftwareSerial.h>
+
+#include <XBee.h>
+
+SoftwareSerial XBee(2, 3); // RX, TX
+
+void setup()
+{
+  XBee.begin(9600);
+  Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+void loop()
+{
+  if (XBee.available())
+    Serial.write(XBee.read());
+  }
 }
