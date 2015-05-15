fcf1984481718ea3f58872f84809852a27640977
Rojam06 <rojam06@gmail.com>
2015-05-15 00:45:24 -0500

    Add setup file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/adafruit/Adafruit_Python_BMP.git
+cd Adafruit_Python_BMP
+python setup.py install
+cd ..
+git clone https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code.git
+
